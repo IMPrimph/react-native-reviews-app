04b505f9358f456810ca76eba97643578bb39318
IMPrimph <vishnugajulapalli123@gmail.com>
2021-05-09 12:53:39 +0530

    navigating between screens

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import { globalStyles } from '../styles/global'
 
-export default function Home() {
+export default function Home({ navigation }) {
+
+    const pressHandler = () => {
+        navigation.navigate('Details')
+        // navigation.push('Details')
+        // props.navigation.navigate('Details');
+    }
+
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home Screen</Text>
+            <Button title='go to reviews' onPress={pressHandler} />
         </View>
     )
 }
