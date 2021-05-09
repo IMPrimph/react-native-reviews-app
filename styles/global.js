7c68d1f6ed20affac85172c7b21ebd66503bb8e5
IMPrimph <vishnugajulapalli123@gmail.com>
2021-05-09 10:44:49 +0530

    react navigation basic setup using stack navigator

--- styles/global.js
+++ styles/global.js
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native'
 
 export const globalStyles = StyleSheet.create({
     container: {
-        padding: 20,
+        padding: 24,
         flex: 1
     },
     titleText: {
