07c524ec0dfa23c02c026a528e510dedfb7685cd
IMPrimph <vishnugajulapalli123@gmail.com>
2021-05-10 22:02:02 +0530

    adding error messages for invalid form inputs

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -30,7 +30,11 @@ export default function ReviewForm({ addReview }) {
                             placeholder='Review title'
                             onChangeText={props.handleChange('title')}
                             value={props.values.title}
+                            onBlur={props.handleBlur('title')}
                         />
+                        <Text style={globalStyles.errorText}>
+                            {props.touched.title && props.errors.title}
+                        </Text>
 
                         <TextInput
                             multiline
@@ -38,7 +42,11 @@ export default function ReviewForm({ addReview }) {
                             placeholder='Review body'
                             onChangeText={props.handleChange('body')}
                             value={props.values.body}
+                            onBlur={props.handleBlur('body')}
                         />
+                        <Text style={globalStyles.errorText}>
+                            {props.touched.body && props.errors.body}
+                        </Text>
 
                         <TextInput
                             keyboardType='number-pad'
@@ -46,7 +54,11 @@ export default function ReviewForm({ addReview }) {
                             placeholder='Review  (1-5)'
                             onChangeText={props.handleChange('rating')}
                             value={props.values.rating}
+                            onBlur={props.handleBlur('rating')}
                         />
+                        <Text style={globalStyles.errorText}>
+                            {props.touched.rating && props.errors.rating}
+                        </Text>
                         <Button title='Submit' color='maroon' onPress={props.handleSubmit} />
                     </View>
                 )}
