56229b5930d139cda68ee19199596bafc6a86771
IMPrimph <vishnugajulapalli123@gmail.com>
2021-05-10 18:49:22 +0530

    add rating image fro ratingDetails

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,18 +1,41 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image, StyleSheet } from 'react-native'
 import { globalStyles } from '../styles/global'
 import Card from '../shared/card';
 
 export default function ReviewDetails({ route, navigation }) {
     const { title, body, rating } = route.params;
+    let imgPath = `../assets/rating-${rating}.png`
     return (
         <View style={globalStyles.container}>
             <Card>
                 <Text style={globalStyles.titleText}>{title}</Text>
                 <Text style={globalStyles.titleText}>{body}</Text>
-                <Text style={globalStyles.titleText}>{rating}</Text>
+                {/* <Text style={globalStyles.titleText}>{rating}</Text> */}
+                <View style={styles.rating}>
+                    <Text style={globalStyles.titleText}>GameZone Rating: </Text>
+                    <View style={styles.ratingHearts}>
+                        {Array(rating).fill().map(() =>
+                            <Image style={styles.image} source={require('../assets/rating-1.png')} />
+                        )}
+                    </View>
+                    {/* <Image source={require(imgPath)} /> */}
+                </View>
             </Card>
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row',
+        justifyContent: "center",
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee'
+    },
+    ratingHearts: {
+        flexDirection: 'row'
+    }
+})
